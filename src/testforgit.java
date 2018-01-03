c73c177b7e783f2a783c84afe921966ae19797a2
dololty <31910845+DoLolitingyu@users.noreply.github.com>
2018-01-03 22:56:45 +0800

    Update testforgit.java

--- src/testforgit.java
+++ src/testforgit.java
@@ -1,10 +1,11 @@
-
+	
 public class testforgit {
 
 	public static void main(String[] args) {
 		
 		String name = "testforgit";
 		System.out.println(name);
+		String name1 = "I am from web";
 		
 	}
 
